ea6a5d25e129453897d9619ea162774f41a0c8ee
Cenk Kılıç <cenk@kilic.dev>
2020-01-22 00:38:51 +0100

    Added ask renewal flag.

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -8,7 +8,8 @@ rsa_key_size=4096
 data_path="/certs"
 email=$EMAIL #Adding a valid address is strongly recommended
 staging=0 #Set to 1 if you're just testing your setup to avoid hitting request limits
-force_renewal=1 # Set to 1 if you want to obtain certificate for every domain again eventhough they exist
+force_renewal=0 # Set to 1 if you want to obtain certificate for every domain again eventhough they exist
+ask_renewal=0 # Set to 1 if you want to manually configure which domains to process
 
 if [ ! -e "$data_path/options-ssl-nginx.conf" ] || [ ! -e "$data_path/ssl-dhparams.pem" ]; then
   echo "### Downloading recommended TLS parameters ..."
@@ -31,7 +32,7 @@ if [ $staging != "0" ]; then staging_arg="--staging"; fi
 if [ $force_renewal != "0" ]; then force_renewal_arg="--force-renewal"; else force_renewal_arg="--keep-until-expiring"; fi
 
 for domain in "${domains[@]}"; do
-  if [[ ! -f "$data_path/live/$domain/fullchain.pem" || ! -f  "$data_path/live/$domain/privkey.pem" || $force_renewal != "0" ]]; then
+  if [[ ! -f "$data_path/live/$domain/fullchain.pem" || ! -f  "$data_path/live/$domain/privkey.pem" ]]; then
     echo "### Creating dummy certificate for $domain ..."
     path="/etc/letsencrypt/live/$domain"
     mkdir -p "$data_path/live/$domain"
@@ -40,8 +41,9 @@ for domain in "${domains[@]}"; do
         -keyout '$path/privkey.pem' \
         -out '$path/fullchain.pem' \
         -subj '/CN=localhost'" certbot
+    touch "$data_path/live/$domain/.dummy"
   else
-    echo "### Skipping $domain, because the certificates for it already exists and force renewal is not set to true."
+    echo "### Skipping $domain, because the certificates for it already exists."
   fi
   echo
 done
@@ -51,8 +53,9 @@ docker-compose up -d nginx-reverseproxy
 echo
 
 for domain in "${domains[@]}"; do
-  if [[ ! -f "$data_path/live/$domain/fullchain.pem" || ! -f  "$data_path/live/$domain/privkey.pem" || $force_renewal != "0" ]]; then
-    echo "### Deleting dummy certificate for $domain ..."
+  if [[ $ask_renewal != "0" ]]; then read -p "### Do you want to force renewal for $domain [y/N]: " renewal_override; fi
+  if [[ -f "$data_path/live/$domain/.dummy" || ${renewal_override:-"n"} = "y" ]]; then
+    echo "### Deleting certificates for $domain ..."
     docker-compose run --rm --entrypoint "\
       rm -Rf /etc/letsencrypt/live/$domain && \
       rm -Rf /etc/letsencrypt/archive/$domain && \
@@ -60,6 +63,7 @@ for domain in "${domains[@]}"; do
     echo
   fi
 
+  echo "### Will try to check certificate validity."
   echo "### Trying for domain/subdomain name $domain"
   domain_args=""
   domain_args="-d $domain"
